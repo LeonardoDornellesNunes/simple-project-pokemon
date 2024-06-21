0efccd50a1368011b5830f17812682ee63ab50b5
Leonardo Dornelles Nunes <leonardonunesdornelles@gmail.com>
2024-06-21 11:45:11 -0300

    adding nextPokemon function to button

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+var nextPokemonId = 1;
+var nextPokemonIdIncrement;
+var pokemonSprite = document.querySelector(".sprite img");
+var buttonNext = document.querySelector(".button-next");
+
 async function gettingPokemonDataByNumber(numberIdPokemon = 1) {
     const responseFromApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${numberIdPokemon}`);
     const jsonResponseFromApi = await responseFromApi.json();
     return jsonResponseFromApi;
 }
 
-var pokemonSprite = document.querySelector(".sprite img");
+function nextPokemon() {
+    nextPokemonIdIncrement = nextPokemonId + 1;
+    nextPokemonId = nextPokemonIdIncrement;
+    gettingPokemonDataByNumber(nextPokemonId).then(x => pokemonSprite.src = x.sprites.front_default);
+}
 
 gettingPokemonDataByNumber().then(x => pokemonSprite.src = x.sprites.front_default);
+
+buttonNext.addEventListener("click", nextPokemon);
